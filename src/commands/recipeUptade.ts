ebfc7ad6d3a7f567017650ac03f6d9a69ff880bd
jolleyily <muhthoriq73@gmail.com>
2025-08-03 20:16:01 +0700

    refactor: add permission check for recipe update command execution

--- src/commands/recipeUptade.ts
+++ src/commands/recipeUptade.ts
@@ -1,8 +1,13 @@
 import type { Message } from "discord.js";
 import { RecipeUpdateController } from "../controllers/recipeUpdateController";
 
+const OWNER_ID = '692765663470551051'
+
 export class RecipeUpdateCommand {
   static async execute(message: Message) {
+    if (message.author.id !== OWNER_ID) {
+      return message.reply('❌ You do not have permission to use this command.')
+    }
     const msg = await message.reply({ content: '⏳Updating all recipes from wiki...' })
     try {
       const meta = await RecipeUpdateController.updateRecipes()
