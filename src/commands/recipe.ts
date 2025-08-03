296688d817a51fd38d2d9f5d5f38ff7bc1ed5133
jolleyily <muhthoriq73@gmail.com>
2025-08-03 23:00:35 +0700

    feat: improve ux by sendTyping bot

--- src/commands/recipe.ts
+++ src/commands/recipe.ts
@@ -1,16 +1,26 @@
 import type { Message } from "discord.js";
-import type { Meta, Recipe } from "../types/recipe";
+import type { Recipe } from "../types/recipe";
 import { RecipeController } from "../controllers/recipeController";
 import { formatRecipeResponse } from "../utils/formatResponse";
 import { MetaService } from "../services/metaService";
 
 export class RecipeCommand {
   static async execute(message: Message, args: string[]) {
-    const metaService = new MetaService()
+    const channel = message.channel
+
+    if (channel && channel.isTextBased() && channel.isSendable()) {
+      channel.sendTyping()
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 300))
+
     if (args.length === 0) {
       return message.reply('Please provide recipe name, example: `!recipe Salad`')
     }
+
+    const metaService = new MetaService()
     const name = args.join(' ')
+
     try {
       const recipe: Recipe = await RecipeController.getRecipe(name)
       const meta = await metaService.get()
